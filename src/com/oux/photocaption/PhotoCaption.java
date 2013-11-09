c03628ec9f3b59c036ad3a3f98c0c5670de68f9b
Sebastien MICHEL <michoux@gmail.com>
2013-11-09 20:34:54 +0100

    Start photoCaption implementation

---
+++ src/com/oux/photocaption/PhotoCaption.java
@@ -0,0 +1,15 @@
+package com.oux.photocaption;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PhotoCaption extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
